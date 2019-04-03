399f074141e5a8bb37b739f3713ef45004eaac9b
Julien Le Bourg <jlebourg@ad.ctg.lu>
2019-04-03 10:36:22 +0200

    first commit

---
+++ webpage-example/webpage/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './docker.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <br/>
+          <br/>
+          <br/>
+          <p>
+          Bienvenue aux Cours <code>Docker</code> !
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
